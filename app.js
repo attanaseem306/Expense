887468ff00f065a3314c73d3ea54861373ab6845
attanaseem306 <attanaseem306@gmail.com>
2023-08-01 02:48:51 -0700

    first commit

--- app.js
+++ app.js
@@ -1,51 +1,81 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-analytics.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js";
+import { collection, addDoc,getDocs } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js";
 
-function total() {
+const firebaseConfig = {
+    apiKey: "AIzaSyByE2tN_PsLz9wl8JS1wPUp9HRcwMw-0qI",
+    authDomain: "first-352b3.firebaseapp.com",
+    projectId: "first-352b3",
+    storageBucket: "first-352b3.appspot.com",
+    messagingSenderId: "627727661584",
+    appId: "1:627727661584:web:3e87800afbdcf0fc972895",
+    measurementId: "G-99CRDS20VC"
+};
 
-    var take = document.getElementById("range");
-    var finalget = take.value;
-    var getone = document.getElementById("one");
-    getone.textContent = finalget;
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+document.getElementById("second").addEventListener('click',async()=>{
+
+    let Range=document.getElementById("range");
+    let Product=document.getElementById("product");
+    let Cost=document.getElementById("cost");
+    let ul=document.getElementById("ul");
+    
+    try {
+        const docRef = await addDoc(collection(db, "Customer"), {
+            Range: Range.value,
+            Product: Product.value,
+            Cost: Cost.value,
+        });
+        console.log("Document written with ID: ", docRef.id);
+    
+        // id = docRef.id
+    
+    } catch (e) {
+        alert("alert")
+        console.error("Error adding document: ", e);
+    }
+    
+    // console.log(id);
+})
+    
+async function show(){
+    const querySnapshot = await getDocs(collection(db, "Customer"));
+    querySnapshot.forEach((doc) => {
+        let total=doc.data().Range
+        let cost=doc.data().Cost
+        let calculate=total-cost;
+        
+        ul.innerHTML+= ` 
+    <div class="show2">
+        <h3> ${doc.data().Range} </h3>
+        <h3> ${doc.data().Cost} </h3>
+        <h3 class="po"> ${doc.data().Product} </h3>
+        <h3 class="ca"> ${calculate} </h3>
+        <div class="in">
+        <i class="fa-solid fa-trash" onclick='Delete("${doc.id}")'></i>
+        <i  class="fa-solid fa-pen-to-square" onclick='Edit("${doc.id}")'></i>
+        </div>
+    </div>`
+    })
+   
+
 }
 
-function my() {
-    var takeCostAmount = document.getElementById("cost")
-    var getTwo = document.getElementById("two");
-    var finalgetTwo = takeCostAmount.value;
-    getTwo.innerText = finalgetTwo
-
-    var take = document.getElementById("range");
-    var finalget = take.value;
-    var balance = finalget - finalgetTwo;
-
-
-    var getThree = document.getElementById("three");
-    getThree.innerText = balance
-
-    var ul = document.getElementById("ul");
-    var inputValue = document.getElementById("product");
-    var inputValue2 = document.getElementById("cost");
-    var li = document.createElement('li');
-    var delBtn = document.createElement('button');
-    var para = document.createElement("p");
-    var delText = document.createTextNode('DELETE');
-    var takeInput = document.createTextNode(inputValue.value);
-    var takeInput2 = document.createTextNode(inputValue2.value);
+show()
+// .then(()=>{
+//     location.reload()
+// })
+// function reload(){
+   
+// }
+// reload()
 
 
-    para.appendChild(takeInput2);
-    delBtn.appendChild(delText);
 
-    li.appendChild(para);
-    li.appendChild(takeInput);
-    inputValue.value = "";
-    inputValue2.value = "";
-    li.appendChild(delBtn);
-    ul.appendChild(li);
-    delBtn.setAttribute("onclick", "empty(this)");
-    delBtn.setAttribute("class", "clear")
 
-}
 
-function empty(a) {
-    a.parentNode.remove();
-}
+
