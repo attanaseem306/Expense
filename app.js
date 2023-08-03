c37c4fc107bb36448c2862c9b721caed4631443f
attanaseem306 <attanaseem306@gmail.com>
2023-08-03 02:40:59 -0700

    second

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-analytics.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js";
-import { collection, addDoc,getDocs } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js";
+import { collection, addDoc, getDocs, doc, deleteDoc ,updateDoc} from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyByE2tN_PsLz9wl8JS1wPUp9HRcwMw-0qI",
@@ -17,64 +17,113 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const db = getFirestore(app);
 
-document.getElementById("second").addEventListener('click',async()=>{
+document.getElementById("second").addEventListener('click', async () => {
 
-    let Range=document.getElementById("range");
-    let Product=document.getElementById("product");
-    let Cost=document.getElementById("cost");
-    let ul=document.getElementById("ul");
-    
+    let Range = document.getElementById("range");
+    let Product = document.getElementById("product");
+    let Cost = document.getElementById("cost");
+    let ul = document.getElementById("ul");
+
+
+
     try {
         const docRef = await addDoc(collection(db, "Customer"), {
             Range: Range.value,
             Product: Product.value,
             Cost: Cost.value,
-        });
+            cal: Range.value - Cost.value,
+        }).then(()=>{
+            Swal.fire({
+                title: `List Added`,
+                icon: 'success'
+            }).then(()=>{
+                location.reload()
+            })
+        })
         console.log("Document written with ID: ", docRef.id);
-    
+
         // id = docRef.id
-    
+
     } catch (e) {
-        alert("alert")
+        
         console.error("Error adding document: ", e);
     }
-    
+
     // console.log(id);
 })
-    
-async function show(){
+
+async function show() {
     const querySnapshot = await getDocs(collection(db, "Customer"));
     querySnapshot.forEach((doc) => {
-        let total=doc.data().Range
-        let cost=doc.data().Cost
-        let calculate=total-cost;
-        
-        ul.innerHTML+= ` 
-    <div class="show2">
+
+        ul.innerHTML +=
+        ` 
+        <div class="show2">
         <h3> ${doc.data().Range} </h3>
         <h3> ${doc.data().Cost} </h3>
         <h3 class="po"> ${doc.data().Product} </h3>
-        <h3 class="ca"> ${calculate} </h3>
+        <h3 class="ca"> ${doc.data().cal} </h3>
         <div class="in">
         <i class="fa-solid fa-trash" onclick='Delete("${doc.id}")'></i>
-        <i  class="fa-solid fa-pen-to-square" onclick='Edit("${doc.id}")'></i>
+        <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        <i  class="fa-solid fa-pen-to-square" onclick='Edit("${doc.id}")'></i> </button>
         </div>
     </div>`
     })
-   
+
 
 }
 
 show()
-// .then(()=>{
-//     location.reload()
-// })
-// function reload(){
-   
-// }
-// reload()
+
+async function Delete(id) {
+    await deleteDoc(doc(db, "Customer", id))
+        .then(() => {
+            Swal.fire({
+                title: `List Deleted`,
+                icon: 'success'
+            })
+        })
+        // .then(() => {
+            function re(){
+                location.reload()
+            }
+            re()
+        // })
+
+}
+
+window.Delete = Delete
 
 
+
+
+   function Edit(id) {
+    let input1=document.getElementById("input1")
+    let input2=document.getElementById("input2")
+    let input3=document.getElementById("input3")
+    
+    document.getElementById("my").addEventListener('click',async()=>{
+    const cityRef = doc(db, 'Customer', id)
+    
+        
+            await updateDoc(cityRef, {
+                one: input1.value,
+                two:input2.value,
+                three:input3.value,
+                // cal: Range.value - Cost.value,
+            })
+           
+            
+
+    
+
+})
+
+}
+
+
+window.Edit = Edit
 
 
 
